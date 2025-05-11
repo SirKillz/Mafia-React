5b8a236e7046da73527cf8f740bed819630450c2
Nick Killeen <nick@aleaquo.com>
2025-05-11 14:12:16 -0500

    Added logo to welcome page

--- react-mafia/src/components/Welcome.jsx
+++ react-mafia/src/components/Welcome.jsx
@@ -1,4 +1,5 @@
 import "../css/welcome.css"
+import mafiaImage from "../assets/mafia.png"
 
 import NavButton from "./common/NavButton";
 import MafiaGame from "../GameAPI/mafiaGame";
@@ -13,10 +14,17 @@ function WelcomePage() {
     }
 
     return (
-        <div className="welcome">
-            <button onClick={handleDebug}>Debug</button>
-            <NavButton text={"ENJOY"} className="enjoy-button" nextPage={"playerEntry"}/>
-        </div>
+        <>
+            <div className="welcome">
+                <div className="header">
+                    <img src={mafiaImage} alt="" />
+                </div>
+                <div className="button-div">
+                    <button onClick={handleDebug}>Debug</button>
+                    <NavButton text={"ENJOY"} className="enjoy-button" nextPage={"playerEntry"}/>
+                </div>
+            </div>
+        </>
     )
 }
 
