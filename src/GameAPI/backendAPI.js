f0f714331e9d3f600309f4aa3621f5087e2c34a4
Nick Killeen <sirkillz1198@gmail.com>
2025-05-23 08:19:03 -0500

    Created the backendAPI to get the players from the database

---
+++ src/GameAPI/backendAPI.js
@@ -0,0 +1,41 @@
+class BACKENDAPI {
+    constructor() {
+        this.baseUrl = import.meta.env.VITE_BASE_API_URL
+    }
+
+    async _makeGetRequest(endpoint, params = {}) {
+  
+        const url = `${this.baseUrl}/v1/mafia${endpoint}?${new URLSearchParams(params)}`;
+        const headers = {
+          "Content-Type": "application/json",
+          "X-API-KEY": import.meta.env.VITE_API_KEY,
+        }
+  
+        return fetch(url, {
+          method: "GET",
+          headers,
+        });
+      }
+
+    async _makePostRequest(endpoint, body = {}) {
+
+    const url = `${this.baseUrl}/v1/mafia${endpoint}`;
+    const headers = {
+        "Content-Type": "application/json",
+        "X-API-KEY": import.meta.env.VITE_API_KEY
+    }
+    
+    return fetch(url, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body),
+    });
+    }
+
+    async getPlayers() {
+        const response = await this._makeGetRequest("/players");
+        return response.json();
+    }
+}
+
+export default new BACKENDAPI();
