bb2c2cd729c18543c0ab11e6bae5545a1dd94e13
Nick Killeen <nick@aleaquo.com>
2025-05-25 12:30:16 -0400

    Implemented initial routing between the main app and the /stats route

---
+++ src/Stats.jsx
@@ -0,0 +1,5 @@
+function Stats() {
+    return <h1>Test</h1>
+}
+
+export default Stats
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
+import './css/index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
 import App from './App.jsx'
 import { NavProvider } from './contexts/NavContext.jsx'
 
+import Stats from './Stats.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+      <NavProvider>
+        <App />
+      </NavProvider>
+  },
+  {
+    path: "/stats",
+    element: <Stats />
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NavProvider>
-      <App />
-    </NavProvider>
+    <RouterProvider router={router}/>
   </StrictMode>
 )
