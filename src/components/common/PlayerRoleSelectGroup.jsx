b0869560371391da191fde5fcef5e63088c0d973
Nick Killeen <sirkillz1198@gmail.com>
2025-05-29 08:57:54 -0500

    Adjusted the styling on the role select page to clearly identify roles based on colors

--- src/components/common/PlayerRoleSelectGroup.jsx
+++ src/components/common/PlayerRoleSelectGroup.jsx
@@ -1,21 +1,32 @@
 import "../../css/formControls.css"
 
+import { useState } from "react";
+
 import roles from "../../GameAPI/roles"
 import MafiaGame from "../../GameAPI/mafiaGame"
 
 function PlayerRoleSelectGroup({playerObj}) {
+    const [selectorClassRole, setSelectorClassRole] = useState(`role-selector role innocent`);
+
+    function formatRoleClassName(role) {
+        let formattedRole = role.toLowerCase().replace(" ", "-");
+        return `role ${formattedRole}`;
+    }
+
     function handleRoleChange(playerId, newRole) {
         const player = MafiaGame.findPlayerByID(playerId);
         player.updateRole(newRole);
+        const formattedRole = formatRoleClassName(newRole);
+        setSelectorClassRole(`role-selector ${formattedRole}`);
     }
 
 
     return (
         <div className="player-row">
             <p>{playerObj.name}</p>
-            <select className="role-selector" onChange={e => handleRoleChange(playerObj.id, e.target.value)}>
+            <select className={selectorClassRole} onChange={e => handleRoleChange(playerObj.id, e.target.value)}>
                 {roles.allRoles.map(role => {
-                    return <option key={role} value={role}>{role}</option>
+                    return <option className={formatRoleClassName(role)} key={role} value={role}>{role}</option>
                 })}
             </select>
         </div>
